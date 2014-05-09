8f96472d98500c8df5ebdf36564040a65e1c7a54
Harri Paavola <harri.paavola@gmail.com>
2014-05-09 23:30:36 +0300

    style updates

--- templates.php
+++ templates.php
@@ -1,181 +1,117 @@
-<?php
-$default_template = '<!doctype html>
-<html lang="en">
-	<head>
-	<meta charset="utf-8">
-	<meta name="dcterms.created" content="{{date}}">
-	<title>{{title}}</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<form action="search.php" method="get" id="search_form">
-		<fieldset>
-			<legend>Search</legend>
-			<label for="terms">Search terms</label>
-			<input name="terms" id="terms" required="required">
- 			<button type="submit">Search</button>
-		</fieldset>
-	</form>
-	<header>
-		<h1>{{title}}</h1>
-	</header>
-	{{{content_html}}}
-	<footer>
-	</footer>
-</body>
-</html>';
+body {
+  font-family: "Trebuchet MS", Helvetica, Arial, sans-serif;
+  color: #333;
+  width: 75%;
+  max-width: 50em;
+  margin: 1em auto 1em auto;
+  overflow: auto;
+  }
+a:link {
+  color: #2366B6;
+  text-decoration: underline;
+  }
+a:visited {
+  color: #2B1558;
+  text-decoration: underline;
+  }
+a:active {
+  color: #458FC7;
+  text-decoration: underline;
+  }
+a:hover {
+  color: #7A0000;
+  text-decoration: none;
+  }
+h1, h2, h3, h4, h5, h6 {
+  font-family: Georgia, "Times New Roman", serif;
+  font-weight: normal;
+  }
+h2, h3, h4, h5, h6 {
+  color: #663534;
+  font-size: 1.1em;
+  margin: 2em 0 0.2em 0;
+  } 
+h1 {
+  font-size: 3em;
+  letter-spacing: 0.2em;
+  border-bottom: 1px solid #333;
+  }
+h2 {
+  padding: 0 0 0.3em 3%;
+  font-size: 1.5em;
+  border-bottom: 1px solid #cecece;
+  text-transform: uppercase;
+  }
+h3{
+  padding: 0 0 0.5em 3%;
+  font-size: 1em;
+  text-transform: uppercase;
+  }
+h4 {
+  padding: 0 0 0.5em 3%;
+  font-size: 1em;
+  }
+h5 {
+  padding: 0 0 0.5em 6%;
+  font-size: 1em;
+  }
+h6 {
+  padding: 0 0 0.5em 9%;
+  font-size: 1em;
+  }
+ul, ol {
+  margin: 1em 2em 1em 2em;
+  padding-left: 1em;
+  }
+ul ul, ol ol, ul ol, ol ul {
+  margin: 0;
+  padding-left: 1em;
+  }
+p {
+  margin: 1em 0 1em 0;
+  }
+legend {
+  font-size: 1.5em;
+  text-transform: uppercase;
+  }
+button {
+  border: 1px outset #333;
+  padding: 0.1em;
+  margin: 0.5em 0 0.2em 0;
+  }
+input, select, textarea {
+  display: block;
+  border: 1px inset #333;
+  padding: 0.1em;
+  width: 100%;
+  margin-bottom: 1em;
+  }
+input[type="checkbox"] {
+  display: inline;
+  width: auto;
+  margin: 5px;
+  }
+pre {
+  margin: 2em;
+  font-family: Consola, monospace;
+  overflow: auto;
+  }
+  
+#outputPane {
+  display: none;
+  }
+#syntaxPane {
+  display: none;
+  }
+#previewPane {
+  overflow: auto;
+  }
 
-$default_template_with_comments = '<!doctype html>
-<html lang="en">
-	<head>
-	<meta charset="utf-8">
-	<meta name="dcterms.created" content="{{date}}">
-	<title>{{title}}</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<form action="search.php" method="get" id="search_form">
-		<fieldset>
-			<legend>Search</legend>
-			<label for="terms">Search terms</label>
-			<input name="terms" id="terms" required="required">
- 			<button type="submit">Search</button>
-		</fieldset>
-	</form>
-	<header>
-		<h1>{{title}}</h1>
-	</header>
-	{{{content_html}}}
-	<ol>
-		{{#comments}}
-		<li id="{{uuid}}">
-			<div class="comment_author">{{{author}}}</div>
-			<div class="comment">{{{comment}}}</div>
-			<div class="comment_time">{{date}} {{time}}</div>
-		</li>
-		{{/comments}}
-	</ol>
-
-	<form action="comments.php" method="post" id="comment_form">
-		<fieldset>
-			<legend>Write new comment</legend>
-			<label for="author">Name and/or email address: </label>
-			<input maxlength="200" name="author" id="author" required="required">
-			<label for="message">Message: </label>
-			<textarea maxlength="10000" id="message" name="message" rows="20" required="required"></textarea>
-	 		<input type="hidden" name="page" value="{{url}}">
-	 		<button type="submit">Comment</button>
-		</fieldset>
-	</form>
-
-	<footer>
-	</footer>
-</body>
-</html>';
-
-$front_page_template = '<!doctype html>
-<html lang="en">
-	<head>
-	<meta charset="utf-8">
-	<title>Spage</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<header>
-		<h1>Spage</h1>
-	</header>
-	{{{content_html}}}
-
-	{{#few_latests}}
-		<h1><a href="{{url}}">{{title}}</a></h1>
-		{{{content_html}}}
-	{{/few_latests}}
-
-	<ol>
-	{{#page_list}}
-		<li><a href="{{url}}">{{title}}</a> ({{date}})</li>
-	{{/page_list}}
-	</ol>
-	<footer>
-	</footer>
-</body>
-</html>';
-
-$search_results_template = '<!doctype html>
-<html lang="en">
-	<head>
-	<meta charset="utf-8">
-	<title>Search results for "{{terms}}"</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<header>
-		<h1>Search results for "{{terms}}"</h1>
-	</header>
-	{{#dropped}}
-		<p>Following search terms were not used since they are under 3 characters: {{terms}}</p>
-	{{/dropped}}
-	{{#sliced}}
-		<p>Only first 10 search terms were used. Follwing were not used: {{terms}}</p>
-	{{/sliced}}
-	<ol>
-	{{#results}}
-		<li><a href="{{url}}.html">{{title}}</a> ({{date}})</li>
-	{{/results}}
-	</ol>
-	<form action="search.php" method="get">
-		<fieldset>
-			<legend></legend>
-			<label for="terms">Search terms</label>
-			<input name="terms" id="terms" required="required" value="{{orig_terms}}">
-		</fieldset>
-		<fieldset>
-			<button type="submit">Submit</button>
-		</fieldset>
-	</form>
-	<footer>
-	</footer>
-</body>
-</html>';
-
-$search_template = '<!doctype html>
-<html lang="en">
-	<head>
-	<meta charset="utf-8">
-	<title>Search</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<header>
-		<h1>Search</h1>
-	</header>
-	<form action="search.php" method="get">
-		<fieldset>
-			<legend></legend>
-			<label for="terms">Search terms</label>
-			<input name="terms" id="terms" required="required">
-		</fieldset>
-		<fieldset>
-			<button type="submit">Submit</button>
-		</fieldset>
-	</form>
-	<footer>
-	</footer>
-</body>
-</html>';
-
-$rss_template = '<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0">
-	<channel>
-		<title>Spage</title>
-		<description>Spage news feed</description>
-		<link>http://spage.example</link>
-		{{#pages}}
-		<item>
-			<title>{{title}}</title>
-			<description>{{content_html}}</description>
-			<link>http://spage.example/{{url}}</link>
-		</item>
-		{{/pages}}
-	</channel>
-</rss>';
+#admin .comment {
+  border: 1px dashed #333;
+  margin: 0.5em;
+  padding: 0.5em;
+}
+#admin input[readonly="readonly"] {
+  background-color: #ccc;
+}
